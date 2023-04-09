8464d97e2dd42742421149c6e45f0a4e7a7cc874
Hyeon-Do Lee <48902147+leehyeondol@users.noreply.github.com>
2023-04-09 22:44:39 +0900

    Add files via upload

---
+++ HW8_2.java
@@ -0,0 +1,78 @@
+package com.example.hw_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPrev,btnNext;
+    myPictrueView mypicture;
+    int curNum=1;
+    File[] imageFiles;
+    String imageFname;
+    TextView textView1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 이미지뷰어");
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
+        btnPrev=(Button)findViewById(R.id.btnPrev);
+        btnNext=(Button)findViewById(R.id.btnNext);
+        textView1=(TextView)findViewById(R.id.textview1);
+        mypicture=(myPictrueView)findViewById(R.id.myPictureView1);
+        imageFiles= new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures").listFiles();
+        imageFname=imageFiles[0].toString();
+        mypicture.imagePath=imageFname;
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(curNum<=1)
+                {
+                    curNum=imageFiles.length;
+                    imageFname=imageFiles[curNum-1].toString();
+                    mypicture.imagePath=imageFname;
+                    mypicture.invalidate();
+                    textView1.setText(String.valueOf(curNum)+"/"+String.valueOf(imageFiles.length));
+                    //Toast.makeText(getApplicationContext(),"첫번쨰 그립입니다.",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    curNum--;
+                    imageFname=imageFiles[curNum-1].toString();
+                    mypicture.imagePath=imageFname;
+                    mypicture.invalidate();
+                    textView1.setText(String.valueOf(curNum)+"/"+String.valueOf(imageFiles.length));
+                }
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(curNum>=imageFiles.length)
+                {
+                    curNum=1;
+                    imageFname=imageFiles[curNum-1].toString();
+                    mypicture.imagePath=imageFname;
+                    mypicture.invalidate();
+                    textView1.setText(String.valueOf(curNum)+"/"+String.valueOf(imageFiles.length));
+                    //Toast.makeText(getApplicationContext(),"마지막 그림입니다.",Toast.LENGTH_SHORT).show();
+                }
+                else {curNum++;
+                    imageFname=imageFiles[curNum-1].toString();
+                    mypicture.imagePath=imageFname;
+                    mypicture.invalidate();}
+                textView1.setText(String.valueOf(curNum)+"/"+String.valueOf(imageFiles.length));
+            }
+        });
+    }
+}
